9e34a245e4f9c058864117b39e6430188280268e
jrc04 <jchuang71@gmail.com>
2026-04-29 16:17:20 -0700

    Added second item. It may mean nothing to y'all, but understand nothing was done for me.

--- src/main/java/net/element/firstmod/item/ModItems.java
+++ src/main/java/net/element/firstmod/item/ModItems.java
@@ -13,6 +13,7 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     //Items
     public static final Item USELESS_ITEM = registerItem("useless_item", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Firstmod.MOD_ID, "useless_item")))));
+    public static final Item LEBRON_JAMES = registerItem("lebron_james", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Firstmod.MOD_ID, "lebron_james")))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(Firstmod.MOD_ID, name), item);
@@ -22,6 +23,7 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
            entries.add(USELESS_ITEM);
+           entries.add(LEBRON_JAMES);
         });
     }
 }
