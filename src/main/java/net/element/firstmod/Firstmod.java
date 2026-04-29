6dbe4a33768ceaed144dc7f417e8cb47ca6d31b0
jrc04 <jchuang71@gmail.com>
2026-04-29 16:04:51 -0700

    Added first item. One must imagine Sisyphus happy.

--- src/main/java/net/element/firstmod/Firstmod.java
+++ src/main/java/net/element/firstmod/Firstmod.java
@@ -1,5 +1,6 @@
 package net.element.firstmod;
 
+import net.element.firstmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class Firstmod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/element/firstmod/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.element.firstmod.item;
+
+import net.element.firstmod.Firstmod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    //Items
+    public static final Item USELESS_ITEM = registerItem("useless_item", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Firstmod.MOD_ID, "useless_item")))));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Firstmod.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        Firstmod.LOGGER.info("Registering Mod Items for {}", Firstmod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+           entries.add(USELESS_ITEM);
+        });
+    }
+}
