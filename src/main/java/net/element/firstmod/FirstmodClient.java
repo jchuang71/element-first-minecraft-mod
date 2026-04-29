a62f3c9d0c9e419cf8dad4e6ee634fe99860a9fc
jrc04 <jchuang71@gmail.com>
2026-04-29 15:30:59 -0700

    Initial commit

---
+++ src/main/java/net/element/firstmod/FirstmodClient.java
@@ -0,0 +1,11 @@
+package net.element.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
