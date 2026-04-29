31be3a6e7ef4f9e8460b70d9ed90657a1af5d43a
jrc04 <jchuang71@gmail.com>
2026-04-29 15:32:33 -0700

    Added test message

--- src/main/java/net/element/firstmod/Firstmod.java
+++ src/main/java/net/element/firstmod/Firstmod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//Test comment
 public class Firstmod implements ModInitializer {
 	public static final String MOD_ID = "firstmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
